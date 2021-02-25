9c8aba8ba8d5660e42adee357889c0793817ebc3
Youkjin Lee <ukjinlee66@gmail.com>
2021-02-25 15:45:58 +0900

    CPP_8 fin

--- CPP_8/ex02/main.cpp
+++ CPP_8/ex02/main.cpp
@@ -6,8 +6,32 @@
 /*   By: youlee <youlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 03:57:45 by youlee            #+#    #+#             */
-/*   Updated: 2021/02/25 03:57:47 by youlee           ###   ########.fr       */
+/*   Updated: 2021/02/25 15:33:43 by youlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "mutantstack.hpp"
 
+int main()
+{
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3); 
+    mstack.push(5); 
+    mstack.push(737);
+    MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it; 
+    }
+    std::stack<int> s(mstack); 
+    return 0;
+}
--- CPP_8/ex02/mutantstack.hpp
+++ CPP_8/ex02/mutantstack.hpp
@@ -10,4 +10,41 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
 
+#include <stack>
+#include <iostream>
+#include <algorithm>
+#include <list>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{    
+    public:        
+        MutantStack() : std::stack<T>() {}
+        MutantStack(const MutantStack &mu) : std::stack<T>(mu) {}
+        MutantStack(const std::stack<T> &st) : std::stack<T>(st) {}
+        MutantStack &operator=(const MutantStack &mu)
+        {
+            if (this == &mu)
+                return (*this);
+            this->c = mu.c;
+            return (*this);
+        }
+        virtual ~MutantStack() {}
+        //iterator function
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+        typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+        typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+        iterator begin(){ return ( this->c.begin()); }
+        iterator end() { return ( this->c.end()); }
+        const_iterator begin() const { return ( this->c.begin()); }
+        const_iterator end() const { return ( this->c.end()); }
+        reverse_iterator rbegin() { return ( this->c.rbegin()); }
+        reverse_iterator rend() { return ( this->c.rend()); }
+        const_reverse_iterator rbegin() const { return ( this->c.rbegin()); }
+        const_reverse_iterator rend() const { return ( this->c.rend()); }
+};
+#endif
